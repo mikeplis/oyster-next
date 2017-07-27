888996392552ff4689b1b7fd732f98db6c27c92e
Mike Plis <michael.plis01@gmail.com>
2017-07-27 08:18:57 -0400

    WIP add meta pricing to hotel page

--- pages/hotel.js
+++ pages/hotel.js
@@ -1,12 +1,13 @@
 import Layout from '../components/Layout';
 import Hotel from '../components/Hotel';
 import withData from '../lib/withData';
+import { initStore } from '../lib/store';
 
 const HotelPage = props => {
     const hotelId = props.url.query.id;
     return (
         <Layout>
-            <Hotel id={hotelId} />
+            <Hotel id={hotelId} store={initStore()} />
         </Layout>
     );
 };
